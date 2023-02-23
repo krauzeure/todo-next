4591dcd162fb221cef6995ff04aefe8381025185
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-23 21:46:23 +0100

    Creating list state

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import ListItem from '../ListItem/ListItem'
+import React from 'react';
+import ListItem from '../ListItem/ListItem';
+import { useState, useEffect } from 'react';
+import { listItem } from '@/types/types';
 
 export default function List() {
+
+    const [items, setItems] = useState<listItem[]>()
+    useEffect(() => {
+        setItems([{ name: "test", id:"lol"}])
+        console.log(items)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
   return (
-    <ul><ListItem /></ul>
-  )
+    <ul>
+      <ListItem />
+    </ul>
+  );
 }
