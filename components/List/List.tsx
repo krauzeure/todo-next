534306198f96b9b1fc5cc73b824f390dfba07c92
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-23 22:42:00 +0100

    Adding style to the hp and the list

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import ListItem from '../ListItem/ListItem';
 import { useState, useEffect } from 'react';
 import { listItem } from '@/types/types';
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import styles from './List.module.css'
 
 export default function List() {
-
-    const [items, setItems] = useState<listItem[]>([])
-    useEffect(() => {
-        setItems([{ name: "test", id:"lol"}])
-        console.log(items)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    console.log(items)
+  const [items, setItems] = useState<listItem[]>([]);
 
   return (
-
-    <ul>
-        {items && items.map((item, index) => {
-        return <ListItem key={item.id}/>})}
+    <ul className={styles.list}>
+      {items &&
+        items.map((item, index) => {
+          return <ListItem key={item.id} name={item.name}/>;
+        })}
     </ul>
-
   );
 }
--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './ListItem.module.css'
+import { Nunito } from '@next/font/google'
 
-export default function ListItem() {
+// If loading a variable font, you don't need to specify the font weight
+const nunito = Nunito({ subsets: ['latin'] })
+
+export default function ListItem(props: {name:string}) {
   return (
-    <div>ListItem</div>
+    <li className={styles.listItem}><span>{props.name}</span><button className={nunito.className}>Supprimer</button></li>
   )
 }
