0b56b7a1d73742a1373ed0c5719bdc7bb270a52c
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 00:28:07 +0100

    Creating our listContext with afunction with a return for now

---
+++ Context/ListContext.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { listContext } from '@/types/types';
+import { listObject } from '@/types/types';
+
+export const GlobalListContext = React.createContext<listContext>({
+  list: [],
+  updateList: () => {},
+});
+
+// @ts-ignore
+export const GlobalListProvider = (props) => {
+  const [list, setList] = useState<listObject[]>([]);
+
+  return (
+    <GlobalListContext.Provider
+      value={{
+        list: list,
+        updateList: setList,
+      }}>
+      {props.children}
+    </GlobalListContext.Provider>
+  );
+};
