88f8c728f621573ff5dd059b3090d93df014936e
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 00:27:25 +0100

    Removing map for now and getting the context value as a test

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ListItem from '../ListItem/ListItem';
 import { useState, useEffect } from 'react';
-import { listItem } from '@/types/types';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './List.module.css'
+import { useContext } from "react";
+import {GlobalListContext} from '../../Context/ListContext'
 
 export default function List() {
-  const [items, setItems] = useState<listItem[]>([]);
+
+    const listValue = useContext(GlobalListContext);
+    console.log(listValue)
 
   return (
-    <ul className={styles.list}>
-      {items &&
-        items.map((item, index) => {
-          return <ListItem key={item.id} name={item.name}/>;
-        })}
-    </ul>
+    <ul></ul>
+    // <ul className={styles.list}>
+    //   {items &&
+    //     items.map((item, index) => {
+    //       return <ListItem key={item.id} name={item.name} />;
+    //     })}
+    // </ul>
   );
 }
