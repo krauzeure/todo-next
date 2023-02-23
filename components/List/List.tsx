e8bf6788e269673b52b99ab7289a7faa187540e0
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-23 22:12:04 +0100

    Fixing state declaration

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import ListItem from '../ListItem/ListItem';
 import { useState, useEffect } from 'react';
 import { listItem } from '@/types/types';
+import { v4 as uuidv4 } from 'uuid'
 
 export default function List() {
 
-    const [items, setItems] = useState<listItem[]>()
+    const [items, setItems] = useState<listItem[]>([])
     useEffect(() => {
         setItems([{ name: "test", id:"lol"}])
         console.log(items)
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+    console.log(items)
 
   return (
+
     <ul>
-      <ListItem />
+        {items && items.map((item, index) => {
+        return <ListItem key={item.id}/>})}
     </ul>
+
   );
 }
