3afb5c2eb07183521c7541ef43c656b0abbfbd7a
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 23:40:38 +0100

    Adding theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { GlobalListProvider } from '@/Context/ListContext';
+import { ThemeProvider } from '@/Context/ThemeContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalListProvider>
+    <ThemeProvider>
       <Component {...pageProps} />
+    </ThemeProvider>
     </GlobalListProvider>
   );
 }
