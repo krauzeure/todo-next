7c6b20fbe49cdd07cfcfb6bd3edaba73831d9dfb
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 23:33:42 +0100

    Creating theme data file

---
+++ data/Themes.tsx
@@ -0,0 +1,21 @@
+import { theme } from "@/types/types"
+
+export const themes: theme[] = [
+    {
+    "name": "classic",
+    "primary": "#FF715B",
+    "secondary": "#4C5B5C",
+    "tertiary": "#F9CB40"
+    },
+    {
+    "name": "midnight",
+    "primary": "blue",
+    "secondary": "#fff",
+    "tertiary": "green"
+    },
+    {
+    "name": "dark",
+    "primary": "blue",
+    "secondary": "#fff",
+    "tertiary": "green"
+    }]
