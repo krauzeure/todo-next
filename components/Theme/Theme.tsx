eb3d105f50ea275a14ada9e92e5f3cd4de8a8228
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 23:49:42 +0100

    Adding onclick func to update theme in context

--- components/Theme/Theme.tsx
+++ components/Theme/Theme.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import styles from './Theme.module.css'
-import { theme } from '@/types/types'
+import React from 'react';
+import styles from './Theme.module.css';
+import { theme } from '@/types/types';
 
-export default function Theme(props: {theme:theme}) {
+import { useContext } from 'react';
+import { ThemeContext } from '@/Context/ThemeContext';
+
+export default function Theme(props: { theme: theme }) {
+  const { theme, updateTheme } = useContext(ThemeContext);
+
+  const editTheme = () => {
+    updateTheme(props.theme.name)
+  }
 
   return (
-    <div className={styles.Theme}>
-        <div className={styles.ThemeColor} style={{"background": props.theme.primary}}></div>
-        <div className={styles.ThemeColor} style={{"background": props.theme.secondary}}></div>
-        <div className={styles.ThemeColor} style={{"background": props.theme.tertiary}}></div>
+    <div className={theme === props.theme.name ? `${styles.Theme} ${styles.SelectedTheme}` : `${styles.Theme}`} onClick={editTheme}>
+      <div
+        className={styles.ThemeColor}
+        style={{ background: props.theme.primary }}></div>
+      <div
+        className={styles.ThemeColor}
+        style={{ background: props.theme.secondary }}></div>
+      <div
+        className={styles.ThemeColor}
+        style={{ background: props.theme.tertiary }}></div>
     </div>
-  )
+  );
 }
