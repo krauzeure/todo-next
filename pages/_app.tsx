c72d58bf0fa901960084373f7ced4be0caf76a90
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 08:53:17 +0100

    Testing context on __app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-import { GlobalListProvider } from '@/Context/ListContext'
+import { GlobalListProvider } from '@/context/ListContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-  <GlobalListProvider>
-     <Component {...pageProps} />
-  </GlobalListProvider>
-  )
+  return (
+    <GlobalListProvider>
+      <Component {...pageProps} />
+    </GlobalListProvider>
+  );
 }
