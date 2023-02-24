f9213378c024c53d7f721853abc016d99527c3be
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-25 00:30:39 +0100

    Creating 3 different themes

--- data/Themes.tsx
+++ data/Themes.tsx
@@ -2,14 +2,20 @@ import { theme } from "@/types/types"
 
 export const themes: theme[] = [
     {
-    "name": "classic",
-    "primary": "#FF715B",
-    "secondary": "#4C5B5C",
-    "tertiary": "#F9CB40"
+        "name": "classic",
+        "primary": "#333",
+        "secondary": "#198f38",
+        "tertiary": "#b72121"
     },
     {
-    "name": "midnight",
-    "primary": "blue",
-    "secondary": "#fff",
-    "tertiary": "green"
+        "name": "midnight",
+        "primary": "#002853",
+        "secondary": "#73286F",
+        "tertiary": "#511C51"
+    },
+    {
+        "name": "german",
+        "primary": "#4C5B5C",
+        "secondary": "#FF715B",
+        "tertiary": "#F9CB40"
     }]
