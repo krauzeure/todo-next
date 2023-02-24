2dff57554be6df3d8ceab7e2b4275f4fdda1ae99
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-24 08:53:44 +0100

    Destructuring the context value and mapping on our list array from the context

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -2,22 +2,19 @@ import React from 'react';
 import ListItem from '../ListItem/ListItem';
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import styles from './List.module.css'
-import { useContext } from "react";
-import {GlobalListContext} from '../../Context/ListContext'
+import styles from './List.module.css';
+import { useContext } from 'react';
+import { GlobalListContext } from '../../context/ListContext';
 
 export default function List() {
-
-    const listValue = useContext(GlobalListContext);
-    console.log(listValue)
+  const {list, updateList} = useContext(GlobalListContext);
 
   return (
-    <ul></ul>
-    // <ul className={styles.list}>
-    //   {items &&
-    //     items.map((item, index) => {
-    //       return <ListItem key={item.id} name={item.name} />;
-    //     })}
-    // </ul>
+    <ul className={styles.list}>
+      {list &&
+        list.map((item, index) => {
+          return <ListItem key={item.id} name={item.name} />;
+        })}
+    </ul>
   );
 }
