632289a6a4ef8909d04ff3f5e86bf37f26a41f75
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-25 16:06:04 +0100

    Adding styles

--- data/Themes.tsx
+++ data/Themes.tsx
@@ -3,9 +3,15 @@ import { theme } from "@/types/types"
 export const themes: theme[] = [
     {
         "name": "classic",
-        "primary": "#333",
+        "primary": "#e0e0df",
         "secondary": "#198f38",
         "tertiary": "#b72121"
+    },
+    {
+        "name": "dark",
+        "primary": "#333",
+        "secondary": "#14712c",
+        "tertiary": "#971b1b"
     },
     {
         "name": "midnight",
@@ -14,8 +20,20 @@ export const themes: theme[] = [
         "tertiary": "#511C51"
     },
     {
-        "name": "german",
-        "primary": "#4C5B5C",
-        "secondary": "#FF715B",
-        "tertiary": "#F9CB40"
+        "name": "green",
+        "primary": "#EDF1D6",
+        "secondary": "#609966",
+        "tertiary": "#40513B"
+    },
+    {
+        "name": "beach",
+        "primary": "#EEE9DA",
+        "secondary": "#93BFCF",
+        "tertiary": "#6096B4"   
+    },
+    {
+        "name": "cream",
+        "primary": "#FAE3D9",
+        "secondary": "#FFB6B9",
+        "tertiary": "#BBDED6"   
     }]
